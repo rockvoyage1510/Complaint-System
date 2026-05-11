d744988348afd78425a950a22f38f19705bce2d2
rockvoyage1510 <nikhil1510patil@gmail.com>
2026-05-11 21:35:45 +0530

    Update server.js

--- server.js
+++ server.js
@@ -15,10 +15,7 @@ const db = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    ssl: {
-        rejectUnauthorized: true
-    }
+    database: process.env.DB_NAME
 });
 
 db.connect((err) => {

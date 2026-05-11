758bb0cd7d77aec813d928e94bbddfeb62d6e601
rockvoyage1510 <nikhil1510patil@gmail.com>
2026-05-11 21:35:16 +0530

    Update server.js

--- server.js
+++ server.js
@@ -314,8 +314,8 @@ app.post('/update-status', (req, res) => {
 // START SERVER
 // ======================
 
-const PORT = process.env.PORT || 3306;
+const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on ${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server running on ${PORT}`);
 });
